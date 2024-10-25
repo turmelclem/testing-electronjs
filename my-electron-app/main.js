99ad2394c35b700351ea97d57977bcbafa1b3375
Clément TURMEL <cturmel@palo-it.com>
2024-10-25 14:55:59 +0200

    add specific OS code to open/close program when windows are closed

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -11,4 +11,18 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+
+  // Open a window if none are open (macOS)
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
